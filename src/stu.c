5679eb3eef608d0a527e27585ad48ccefbc28522
Ssdj3nt <92294163+Ssdj3nt@users.noreply.github.com>
2024-09-05 19:15:21 +0200

    Update stu.c

--- src/stu.c
+++ src/stu.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
+#include <stdio.h> // Fornisce funzioni per l'input/output.
+#include <stdlib.h>  // Libreria che include funzioni per la gestione della memoria dinamica
+#include <string.h> // Libreria per la manipolazione di stringhe.
+#include <unistd.h> // Fornisce l'accesso alle system-call su sistemi UNIX.
+#include <arpa/inet.h> // Libreria per la gestione delle connessioni di rete su sistemi UNIX.
 
-#define SERVER_IP "127.0.0.1"
-#define SECRETARY_PORT 8081
-#define BUFFER_SIZE 1024
+#define SERVER_IP "127.0.0.1" // Indirizzo IP della segreteria.
+#define SECRETARY_PORT 8081 // Porta di comunicazione della Segreteria.
+#define BUFFER_SIZE 1024 // Size del buffer.
 
-void inviaRichiestaAllaSegreteria(char tipoRichiesta, char *matricola, char *nomeEsame, char *dataEsame) {
-    int sock_segreteria;
-    struct sockaddr_in server_addr_segreteria;
-    char buffer[BUFFER_SIZE] = {0};
+void inviaRichiestaAllaSegreteria(char tipoRichiesta, char *matricola, char *nomeEsame, char *dataEsame) { // Funzione per la gestione della richiesta degli appelli disponibili.
+    int sock_segreteria; // Socket della segreteria.
+    struct sockaddr_in server_addr_segreteria; // Indirizzo della Segreteria.
+    char buffer[BUFFER_SIZE] = {0}; 
 
     // Creazione del socket per la connessione con la segreteria
     if ((sock_segreteria = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -19,10 +19,10 @@ void inviaRichiestaAllaSegreteria(char tipoRichiesta, char *matricola, char *nom
         exit(EXIT_FAILURE);
     }
 
-    server_addr_segreteria.sin_family = AF_INET;
-    server_addr_segreteria.sin_port = htons(SECRETARY_PORT);
+    server_addr_segreteria.sin_family = AF_INET; // Si specifica che l'indirizzo del server utilizzerà l'indirizzamento ipv4.
+    server_addr_segreteria.sin_port = htons(SECRETARY_PORT); // Converte il numero di porta della Segreteria.
 
-    if (inet_pton(AF_INET, SERVER_IP, &server_addr_segreteria.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, SERVER_IP, &server_addr_segreteria.sin_addr) <= 0) { // Conversione dell'indirizzo IP del server da formato testuale a formato binario.
         perror("Indirizzo non valido o non supportato");
         exit(EXIT_FAILURE);
     }
@@ -33,23 +33,23 @@ void inviaRichiestaAllaSegreteria(char tipoRichiesta, char *matricola, char *nom
         exit(EXIT_FAILURE);
     }
 
-    // Invia la richiesta alla segreteria
+    // Invia la richiesta alla segreteria.
     if (tipoRichiesta == 'P') {
-        snprintf(buffer, sizeof(buffer), "%c%s %s %s", tipoRichiesta, matricola, nomeEsame, dataEsame);
+        snprintf(buffer, sizeof(buffer), "%c%s %s %s", tipoRichiesta, matricola, nomeEsame, dataEsame); // Viene inserito nel buffer la tipologia della richiesta, Esame e Data esame. 
     } else {
-        snprintf(buffer, sizeof(buffer), "%c%s", tipoRichiesta, nomeEsame);
+        snprintf(buffer, sizeof(buffer), "%c%s", tipoRichiesta, nomeEsame); // Viene inserito nel buffer la tipologia della richiesta ed il nome esame,
     }
 
-    send(sock_segreteria, buffer, strlen(buffer), 0);
+    send(sock_segreteria, buffer, strlen(buffer), 0); // Invio dei dati contenuti in buffer alla Segreteria.
 
-    // Ricevi la risposta
+    // Lo studente riceve risposta dell'opportuna richiesta fatta.
     int len = read(sock_segreteria, buffer, BUFFER_SIZE);
     if (len > 0) {
         buffer[len] = '\0';
         printf("%s\n", buffer);
     }
 
-    close(sock_segreteria);
+    close(sock_segreteria); //Chiusura della connessione con la Segreteria.
 }
 
 int main() {
